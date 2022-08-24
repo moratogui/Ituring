296d24d3bd36d69000d8648a4afeb2923453ab80
Gui Morato <guilherme.mms@gmail.com>
2022-08-24 19:24:16 -0300

    atualização-pratica1+desafio

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,11 +1,8 @@
 let nome = "Guilherme"
 let email = "guilherme.mms@gmail.com"
-let idade = 32
+let idade = 32;
 let profissão = "Geofísico"
 let lugar = "Islândia"
 
-console.log(nome)
-console.log(email)
-console.log(idade)
-console.log(profissão)
-console.log(lugar)
+var texto = "Olá, " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + lugar + ". Confirma pra gente alguns dados? A sua idade é " + idade + " , você é " + profissão + " e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
+console.log(texto)

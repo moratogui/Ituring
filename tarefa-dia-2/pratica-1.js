78449dceb0c9e9dbe54b51e9071710023e0459f3
Gui Morato <guilherme.mms@gmail.com>
2022-08-24 18:56:13 -0300

    atualizacao-pratica-1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,9 @@ let email = "guilherme.mms@gmail.com"
 let idade = 32
 let profissão = "Geofísico"
 let lugar = "Islândia"
+
+console.log(nome)
+console.log(email)
+console.log(idade)
+console.log(profissão)
+console.log(lugar)
